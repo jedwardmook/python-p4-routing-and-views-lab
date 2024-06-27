97b08c1f3c4f22caf83ed8149d6fb4a4271214a0
JedwardMook <jedwardmook@yahoo.com>
2024-06-27 15:49:21 -0500

    tests passing

--- server/app.py
+++ server/app.py
@@ -4,5 +4,34 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+  return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+  print(parameter)
+  return f'{parameter}'
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+  def count_to_parameter():
+    for n in range(parameter):
+      yield f'{n}\n'
+  return count_to_parameter()
+
+@app.route('/math/<num1>/<operation>/<num2>')
+def math(num1, operation, num2):
+  result = 0
+  if operation == "+":
+    result = (int(num1) + int(num2))
+  elif operation == "-":
+    result = (int(num1) - int(num2))
+  elif operation == "*":
+    result = (int(num1) * int(num2))
+  elif operation == "div":
+    result = (int(num1) / int(num2))
+  return str(result)
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+  app.run(port=5555, debug=True)
